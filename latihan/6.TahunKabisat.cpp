9457faa83644b8cf736614b9ccd2ec2572580a2d
aazziqrah-collab <aazziqrah@gmail.com>
2025-08-15 12:32:11 +0700

    Update 6.TahunKabisat.cpp

--- latihan/6.TahunKabisat.cpp
+++ latihan/6.TahunKabisat.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+bool isleapYear (int year) {
+  // Tahun kabisat jika:
+  // 1. habis dibagi 4 tetapi tidak habis dibagi 100, ATAU
+  // 2. Habis dibagi 400
+  return (year % 4 == 0 && year % 100 != 0) \|| (year % 400 == 0);
+}
+int main () {
+  int year;
+cout << "Masukkan tahun: ";
+cin >> year;
+     if (isleapYear(year)} {
+    cout << year << " adalah tahun kabisat." << endl;
+} else {
+    cout << year << " bukan tahun kabisat." << endl;
+}
+    return 0;
+}
