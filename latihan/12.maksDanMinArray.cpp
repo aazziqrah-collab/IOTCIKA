5eff309501dc8f638ed4f18a0c729c8a22025ba8
aazziqrah-collab <aazziqrah@gmail.com>
2025-08-22 13:43:19 +0700

    Update 12.maksDanMinArray.cpp

--- latihan/12.maksDanMinArray.cpp
+++ latihan/12.maksDanMinArray.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+ int main() {
+   int n;
+
+cout << "PROGRAM MENCARI NILAI MAKSIMUM DAN MINIMUM" << endl;
+cout << "==========================================" << endl;
+
+//Input jumlah elemen array
+cout << "Masukkan jumlah elemen array: ";
+cin >> n;
+
+int arr[n];
+
+//Input elemen array
+cout << "Masukkan " << n << " elemen array: " << endl;
+for (int i = 0; i < n; i++) {
+cout << " Elemen ke-" << i + 1 << ": ";
+cin >> arr [i];
+}
+
+// inisialisasi nilai max dan min
+int max = arr[0];
+int min = arr[0];
+
+// menacari nilai maxium dan minimum
+for (int i = 
