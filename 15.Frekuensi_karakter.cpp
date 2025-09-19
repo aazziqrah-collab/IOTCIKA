4a878ec5f69d029348d6744d1c33cb0c527dd240
aazziqrah-collab <aazziqrah@gmail.com>
2025-09-19 10:36:37 +0700

    Create 15.Frekuensi_karakter.cpp

---
+++ 15.Frekuensi_karakter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+  string teks = "programing";
+  map<char,int> frekuensi;
+
+  for (char c : teks) {
+      frekuensi[c]++;
+  }
+
+cout << "Frekuensi karakter:" << endl;
+for (auto pair : frekuensi {
+  cout << "'" << pair .first << "': " << pair .second << endl;
+}
+return 0;
+}
