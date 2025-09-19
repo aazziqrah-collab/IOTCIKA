a5ce41d56fdcfae94c5e2a791b11214254dec197
aazziqrah-collab <aazziqrah@gmail.com>
2025-09-19 10:48:06 +0700

    Update 16.Membali_string.cpp

--- 16.Membali_string.cpp
+++ 16.Membali_string.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    string teks;
+    cout << "Masukkan sebuah string: ";
+    getline(cin, teks);
+    reverse(teks,begin());
+    cout << "Hasil string setelah dibalik: " << teks << endl;
+    return 0;
+    }
