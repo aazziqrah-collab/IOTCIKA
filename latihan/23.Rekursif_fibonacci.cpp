e04a77e355d4c7646b5a7c6465846b7f50a4728c
aazziqrah-collab <aazziqrah@gmail.com>
2025-09-26 12:56:31 +0700

    Update 23.Rekursif_fibonacci.cpp

--- latihan/23.Rekursif_fibonacci.cpp
+++ latihan/23.Rekursif_fibonacci.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int faktorial(int n) {
-  if (n == 0 || n == 1 ) {
-      return 1;
+int fibonacci(int n) {
+  if (n == 0) {
+      return 0;
+  } else if (n == 1 ) {
+      return 0;
   } else {
-      return n * faktorial(n - 1);
-  }
-}
-
-int main () {
-  int angka;
-  cout << "Masukkan bilangan: ";
-  cin >> angka;
-
-if (angka < 0) {
-    cout << "Faktorial tidak terdifinisi untuk bilangan negatif." << endl;
-} else { 
-     cout << "Faktorial dari " << angka << " adalah: " << faktorial(angka) endl;
-}
-return 0;
-}
+      return fibonacci(n -1) +
