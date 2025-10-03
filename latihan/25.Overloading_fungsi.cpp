9917c877ccc1524ecd4c9f6bf2cca6b4d62f8719
aazziqrah-collab <aazziqrah@gmail.com>
2025-10-03 12:34:23 +0700

    Create 25.Overloading_fungsi.cpp

---
+++ latihan/25.Overloading_fungsi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#using namespace std;
+
+int luas(int sisi) {
+  return sisi * sisi;
+}
+
+int luas(int panjang, int lebar) { 
+    return panjang * lebar;
+}
+
+double luas(double jari) {
+  return 3.34 * jari * jari;
+}
+|
+int main() {
+  int sisi, panjang, lebar;
+  double jari;
+
+cout << "Masukkan sisi persegi : ";
+cin >> sisi;
+cout << "Luas persegi =" << luas(sisi) << endl;
+
+cout << "\nMasukkan panjang persegi panjang : ";
+cin >> panjang;
+cout << "Masukkan lebar panjang : ";
+cin >> lebar;
+cout << "Luas persegi panjang = " << luas(panjang, lebar) << endl;
+
+cout << "\nMasukkan jari-jari lingkaran : ";
+cin >> jari;
+cout << "Luas lingkaran =" << Luas(jari) << endl;
+
+return 0;
+}
