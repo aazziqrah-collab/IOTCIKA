27831904c583ad31cadb9d7f5a1c5d36b767b2f5
aazziqrah-collab <aazziqrah@gmail.com>
2025-10-03 13:03:58 +0700

    Create 26.Pas value vs refrence.cpp

---
+++ latihan/26.Pas value vs refrence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void ubahValue(int a) {
+  a = a + 10;
+  cout << "Nilai di dalam fungsi (pass by value) : " << a << endl;
+}
+void ubahReference(int b) {
+  b = b + 10;
+  cout << "Nilai di dalam fungsi (pass by reference) : " << b << endl;
+}
+
+int main() {
+  int x = 5;
+  int y = 5;
+  cout << "Sebelum fungsi di panggil:" << endl;
+  cout << "x = " << x << " (unutk pass by value)" << endl;
+  cout << "y = " << y << " (unutk pass by reference)" << endl;
+
+  cout << "\nMemanggil fungsi pass by value..." << endl;
+  ubahValue(x);
+  cout << "Nilai x setelah fungsi (pass by value) : " << x << endl;
+
+  cout << "\nMemanggil fungsi pass by reference..." << endl;
+  ubahReference(y);
+  cout << "Nilai y setelah fungsi (pass by reference) : " << y << endl;
+
+  return 0;
+}
